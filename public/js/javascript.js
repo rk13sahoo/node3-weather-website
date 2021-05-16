d6d245667516109c49fe41cfe43dc832ec004ea7
unknown <rk13sahoo@gmail.com>
2021-05-16 19:36:41 +0530

    Remove unnecessary console.log call

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,4 +1,4 @@
-console.log('Client side javaScript');
+//console.log('Client side javaScript');
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
